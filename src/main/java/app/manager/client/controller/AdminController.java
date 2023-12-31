57b5494c9e161fa9127e3f2f7d1a76d66c18bc51
GiaHao810 <giahaobui810@gmail.com>
2023-12-31 20:09:58 +0700

    Update product data

--- src/main/java/app/manager/client/controller/AdminController.java
+++ src/main/java/app/manager/client/controller/AdminController.java
@@ -1,31 +1,34 @@
 package app.manager.client.controller;
 
 import app.manager.client.dto.ResponseObject;
-import app.manager.client.model.*;
-import app.manager.client.repository.OrderRepository;
+import app.manager.client.model.Product;
+import app.manager.client.model.Role;
+import app.manager.client.model.User;
 import app.manager.client.repository.ProductRepository;
 import app.manager.client.service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
-@RequestMapping
-public class AppController {
-
+@RequestMapping("/admin")
+public class AdminController {
     @Autowired
     private LoginService loginService;
     @Autowired
     private ProductRepository productRepository;
-    @GetMapping("/")
+    @GetMapping("/home")
     public String getHome(Model model){
         List<Product> productList = productRepository.findAll();
         model.addAttribute("productList", productList);
-        return "home";
+        return "admin/home";
     }
 
     @GetMapping("/login")
---
+++ src/main/java/app/manager/client/controller/ClientController.java
@@ -0,0 +1,23 @@
+package app.manager.client.controller;
+
+import app.manager.client.dto.ResponseObject;
+import app.manager.client.model.Product;
+import app.manager.client.model.Role;
+import app.manager.client.model.User;
+import app.manager.client.repository.ProductRepository;
+import app.manager.client.service.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+@Controller
+@RequestMapping("/")
+public class ClientController {
+
+}
