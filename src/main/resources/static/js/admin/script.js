135412825cf3882c8bcef525740a72fff6580dd4
GiaHao810 <giahaobui810@gmail.com>
2024-04-07 19:49:29 +0700

    Thêm sản phẩm vào Order Giá tự động thay đổi (Xóa - Thêm sản phẩm)

--- src/main/resources/static/js/admin/script.js
+++ src/main/resources/static/js/admin/script.js
@@ -1,27 +1,47 @@
 function handleAddImage(imageInput) {
     var prod = $(imageInput).closest('li');
 
-    if(!isProdInOrder(prod)) addNewProdToOrder(prod);
+    if(!isProdInOrder(prod)) 
+        addNewProdToOrder(prod);
+        handleTotalPriceChange();
 }
 
 function addNewProdToOrder(prod){
-    var prodPrice = prod.find('.product-price').val();
+    var prodPrice = prod.find('input.product-price').val();
     var prodURL = prod.find('.product-image').attr("src");
     var prodName = prod.find('.product-name').text();
 
-    var newProd = $('<li>').addClass('product-item row p-2').append(
-        $('<div>').addClass('col border border-3 rounded').append(
-            $('<label>').addClass('col-md order-name').text(prodName)
+    var newProd = $('<li>')
+    .addClass('product-item row p-2')
+    .append(
+        $('<div>')
+        .addClass('col border border-3 rounded')
+        .append(
+            $('<label>')
+                .addClass('col-md order-name')
+                .text(prodName)
         ),
-        $('<div>').addClass('col').append(
-            $('<input>').addClass('col-md order-quantity border border-3 rounded h-100')
-            .attr('type', 'number').val(1)
+        $('<div>')
+            .addClass('col')
+            .append(
+                $('<input>')
+                    .addClass('col-md order-quantity border border-3 rounded h-100')
+                    .attr('type', 'number')
+                    .attr('min', '1')
+                    .val(1)
+                    .change(handleTotalPriceChange)
         ),
-        $('<div>').addClass('col').append(
-            $('<button>').addClass('col-md delete-button').text('Delete').click(
-                function(){
-                    // delProd
-                }
+        $('<div>')
+        .addClass('col')
+        .append(
+            $('<button>')
+                .addClass('col-md delete-button')
+                .click(function(){
+                    handleDelItemOrder(this)
+                })
+            .append(
+                $('<i>')
+                    .addClass('fa-solid fa-trash')
             )
         ),
         $('<input>').addClass('order-url').attr('type', 'hidden').val(prodURL),
@@ -34,6 +54,7 @@ function addNewProdToOrder(prod){
 function isProdInOrder(prod){
     var prodItems = $('#ordertable').find('li.product-item');
     var isFound = false;
+
     prodItems.each(function(){
         if(prod.find('.product-name').text() === $(this).find('.order-name').text()) 
             isFound = true;
@@ -41,4 +62,51 @@ function isProdInOrder(prod){
     });
 
     return isFound;
+}
+
+function handleDelItemOrder(button){
+    delProdFromOrder(button);
+    handleTotalPriceChange();
+}
+
+function delProdFromOrder(button){
+    $(button).closest('.product-item').remove();
+}
+
+function handleTotalPriceChange(){
+    var prodItems = $('#ordertable').find('li.product-item');
+    var total = 0.0;
+    var curTotal = 0.0;
+
+    if(prodItems.length === 0) {
+        $('#total-price-value').text('0 đ');
+        return;
+    }
+    
+    prodItems.each(function(){
+        curTotal = 
+            $(this)
+            .find('.order-price')
+            .val()
+         * 
+            $(this)
+            .find('.order-quantity')
+            .val()
+        ;
+        total += curTotal;
+    });
+
+    $('#total-price-value').text(formatCurrency(total));
+}
+
+function formatCurrency(number) {
+    // Sử dụng Intl.NumberFormat để định dạng số thành tiền tệ Việt Nam Đồng
+    const formatter = new Intl.NumberFormat('vi-VN', {
+        style: 'currency',
+        currency: 'VND',
+        minimumFractionDigits: 3
+    });
+
+    // Áp dụng định dạng và trả về chuỗi định dạng
+    return formatter.format(number);
 }
