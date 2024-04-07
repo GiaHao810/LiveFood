b64d344a596397c5b27357d80fb8e56223584900
GiaHao810 <giahaobui810@gmail.com>
2024-04-07 17:16:50 +0700

    Thêm Feature handleAddImage(imageInput) Viết lại bằng Jquery

--- src/main/resources/static/js/admin/script.js
+++ src/main/resources/static/js/admin/script.js
@@ -1,25 +1,44 @@
-function getImageLabel(imageInput) {
-    // Lấy đối tượng label tương ứng với input image
-    var listItems = imageInput.closest('li');
-    var imageURL = imageInput.src;
-    var itemPrice = listItems.querySelector('.product-price').value;
-    var labelValue = listItems.querySelector('.product-name').innerText;
+function handleAddImage(imageInput) {
+    var prod = $(imageInput).closest('li');
 
-//  Kiểm tra sản phẩm đã có trên Order Table
-    var itemNames = getValues();
-    var isTrue = false;
+    if(!isProdInOrder(prod)) addNewProdToOrder(prod);
+}
 
-    itemNames.forEach(function(itemName) {
-        if(itemName == labelValue){
-            addQuantity(itemName);
-            adjustTotalPrice(itemPrice, 'add');
-            isTrue = true;
-        }
+function addNewProdToOrder(prod){
+    var prodPrice = prod.find('.product-price').val();
+    var prodURL = prod.find('.product-image').attr("src");
+    var prodName = prod.find('.product-name').text();
+
+    var newProd = $('<li>').addClass('product-item row p-2').append(
+        $('<div>').addClass('col border border-3 rounded').append(
+            $('<label>').addClass('col-md order-name').text(prodName)
+        ),
+        $('<div>').addClass('col').append(
+            $('<input>').addClass('col-md order-quantity border border-3 rounded h-100')
+            .attr('type', 'number').val(1)
+        ),
+        $('<div>').addClass('col').append(
+            $('<button>').addClass('col-md delete-button').text('Delete').click(
+                function(){
+                    // delProd
+                }
+            )
+        ),
+        $('<input>').addClass('order-url').attr('type', 'hidden').val(prodURL),
+        $('<input>').addClass('order-price').attr('type', 'hidden').val(prodPrice)
+    );
+
+    $('#ordertable').append(newProd);
+}   
+
+function isProdInOrder(prod){
+    var prodItems = $('#ordertable').find('li.product-item');
+    var isFound = false;
+    prodItems.each(function(){
+        if(prod.find('.product-name').text() === $(this).find('.order-name').text()) 
+            isFound = true;
+            return;
     });
 
-//  Nếu sản phẩm chưa có thì thêm vào Order Table
-    if(!isTrue){
-        addNewRowItem(labelValue, itemPrice, imageURL);
-        adjustTotalPrice(itemPrice, 'add');
-    }
+    return isFound;
 }
