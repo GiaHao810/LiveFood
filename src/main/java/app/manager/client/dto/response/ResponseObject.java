b303c587eedf8c70c53ceb2093c1449814e811e5
GiaHao810 <giahaobui810@gmail.com>
2024-11-09 22:35:40 +0700

    Upgrade return value

--- src/main/java/app/manager/client/dto/response/ResponseObject.java
+++ src/main/java/app/manager/client/dto/response/ResponseObject.java
@@ -1,22 +1,32 @@
 package app.manager.client.dto.response;
 
 import app.manager.client.dto.Response;
+import jakarta.annotation.Nullable;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
-@EqualsAndHashCode(callSuper = true)
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
-public class ResponseObject extends Response {
+public class ResponseObject<T> {
     private String status;
-    private Object data;
+    private String error;
+    private T data;
 
-    public ResponseObject(String message, String status, Object data){
-        super(message);
+    public ResponseObject(String status, T data){
         this.status = status;
         this.data = data;
+    }
+
+    public ResponseObject(String status, String error, T data){
+        this.status = status;
+        this.error = error;
+        this.data = data;
+    }
+
+    public ResponseObject(String status, String error) {
+        this.status = status;
+        this.error = error;
     }
 }
